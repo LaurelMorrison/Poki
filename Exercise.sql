1e721bd8d96802c7bf26f9c3e962493a6b07669f
Laurel <lbentleym1992@gmail.com>
2021-06-03 12:57:50 -0400

    step 11 complete

--- Exercise.sql
+++ Exercise.sql
@@ -17,20 +17,65 @@ from Poem p;
 --------------------------------------
 4. Sort authors alphabetically by name. What are the names of the top 76 authors?
 
-Select a.Name, a.Id
+Select top 76 a.Name, a.Id
 from Author a
-Where a.id <= 76
 Order by Name
 
 --------------------------------------
 5. Starting with the above query, add the grade of each of the authors.
 
-Select a.Name, a.Id, g.Name
+Select top 76 a.Name, a.Id
 from Author a
 left join Grade g on g.id = a.GradeId
-Where a.id <= 76
 Order by a.Name
 
 --------------------------------------
 6. Starting with the above query, add the recorded gender of each of the authors.
+
+Select top 76  a.Name, a.Id, g.Name
+from Author a
+left join Grade g on g.id = a.GradeId
+left join Gender f on f.id = a.GenderId
+Order by a.Name
+
+--------------------------------------
+7. What is the total number of words in all poems in the database?
+
+Select sum(p.WordCount) '# of words'
+from Poem p;
+
+--------------------------------------
+8. Which poem has the fewest characters?
+
+Select top 1 p.Title, p.CharCount
+From Poem p
+Order by p.CharCount
+
+--------------------------------------
+9. How many authors are in the third grade?
+
+Select count(a.Name) '# of 3rd poets'
+From Author a
+left join Grade g on g.id = a.GradeId
+where g.Name = '3rd Grade'
+
+--------------------------------------
+10. How many total authors are in the first through third grades?
+
+Select count(a.Name) '# of 1st-3rd poets'
+From Author a
+left join Grade g on g.id = a.GradeId
+where g.Name = '1st Grade' OR g.Name = '2nd Grade' OR g.Name = '3rd Grade'
+
+--------------------------------------
+11. What is the total number of poems written by fourth graders?
+
+Select count(p.Title) '# of poems by 4th graders'
+From Author a
+left join Grade g on g.id = a.GradeId
+left join Poem p on p.AuthorId = a.Id
+where g.Name = '4th Grade'
+
+--------------------------------------
+12. How many poems are there per grade?
 */
